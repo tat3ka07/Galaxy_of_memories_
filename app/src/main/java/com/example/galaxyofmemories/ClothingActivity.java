20e4410c747fcc851c749868c457fff32809b864
Tat3ka07 <tatamiraghyan.7@gmail.com>
2024-02-17 02:22:30 +0400

    Galaxy_of_memories + buttons

---
+++ app/src/main/java/com/example/galaxyofmemories/ClothingActivity.java
@@ -0,0 +1,4 @@
+package com.example.galaxyofmemories;
+
+public class ClothingActivity {
+}
--- app/src/main/java/com/example/galaxyofmemories/Register.java
+++ app/src/main/java/com/example/galaxyofmemories/Register.java
@@ -93,7 +93,7 @@ public class Register extends AppCompatActivity {
         signInBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                startActivity(new Intent(Register.this, Login.class));
             }
         });
 
