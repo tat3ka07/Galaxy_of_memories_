05f242b5b29d1d37a443f60968dc1c3cfdf2f80d
tat3ka07 <tatamiraghyan.7@gmail.com>
2024-04-06 01:23:08 +0400

    1 + 2 + 3 + 4 + 5

--- app/src/main/java/com/example/galaxyofmemories/activities/MainActivity2.java
+++ app/src/main/java/com/example/galaxyofmemories/activities/MainActivity2.java
@@ -1,6 +1,9 @@
 package com.example.galaxyofmemories.activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
@@ -11,14 +14,20 @@ import android.view.View;
 import android.widget.ImageView;
 
 import com.example.galaxyofmemories.R;
+import com.example.galaxyofmemories.adapters.NotesAdapter;
 import com.example.galaxyofmemories.database.NotesDatabase;
 import com.example.galaxyofmemories.entities.Note;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity2 extends AppCompatActivity {
 
     public static final int REQUEST_CODE_ADD_NOTE = 1;
+
+    private RecyclerView notesRecyclerView;
+    private List<Note> noteList;
+    private NotesAdapter notesAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +44,16 @@ public class MainActivity2 extends AppCompatActivity {
                 );
             }
         });
+
+        notesRecyclerView = findViewById(R.id.notesRecyclerView);
+        notesRecyclerView.setLayoutManager(
+                new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        );
+
+        noteList = new ArrayList<>();
+        notesAdapter = new NotesAdapter(noteList);
+        notesRecyclerView.setAdapter(notesAdapter);
+
         getNotes();
     }
 
@@ -53,11 +72,25 @@ public class MainActivity2 extends AppCompatActivity {
             @Override
             protected void onPostExecute(List<Note> notes) {
                 super.onPostExecute(notes);
-                Log.d("MY_NOTES", notes.toString());
+                if(noteList.size() == 0) {
+                    noteList.addAll(notes);
+                    notesAdapter.notifyDataSetChanged();
+                } else {
+                    noteList.add(0, notes.get(0));
+                    notesAdapter.notifyItemInserted(0);
+                }
+                notesRecyclerView.smoothScrollToPosition(0);
             }
         }
 
         new GetNotesTask().execute();
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_CODE_ADD_NOTE && resultCode == RESULT_OK) {
+            getNotes();
+        }
+    }
 }
