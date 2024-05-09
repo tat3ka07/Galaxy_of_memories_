9bda1dfc6a873fde134a588689727dada8317176
tat3ka07 <tatamiraghyan.7@gmail.com>
2024-05-09 23:16:44 +0400

    11+, +firebaze

--- app/src/main/java/com/example/galaxyofmemories/Login.java
+++ app/src/main/java/com/example/galaxyofmemories/Login.java
@@ -1,60 +1,121 @@
 package com.example.galaxyofmemories;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.InputType;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
 
-    private boolean passwordShowing = false;
+    EditText loginEmail, loginPassword;
+    Button loginButton;
+    TextView signupRedirectText;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        final EditText usernameET = findViewById(R.id.usernameET);
-        final EditText passwordET = findViewById(R.id.passwordET);
-        final ImageView passwordIcon = findViewById(R.id.passwordIcon);
-        final TextView signUpBtn = findViewById(R.id.signUpBtn);
+        mAuth = FirebaseAuth.getInstance();
 
+        loginEmail = findViewById(R.id.email);
+        loginPassword = findViewById(R.id.password);
+        loginButton = findViewById(R.id.loginBtn);
+        signupRedirectText = findViewById(R.id.signupText);
 
-        passwordIcon.setOnClickListener(new View.OnClickListener() {
+        if (isLoggedIn()) {
+            navigateToMainPage();
+        }
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
+                String email = loginEmail.getText().toString().trim();
+                String password = loginPassword.getText().toString().trim();
 
-                if (passwordShowing) {
-                    passwordShowing = false;
-
-                    passwordET.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
-                    passwordIcon.setImageResource(R.drawable.password_show);
-                } else {
-                    passwordShowing = true;
-
-                    passwordET.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
-                    passwordIcon.setImageResource(R.drawable.password_hide);
+                if (email.isEmpty()) {
+                    loginEmail.setError("Email is required");
+                    return;
                 }
 
-                passwordET.setSelection(passwordET.length());
+                if (password.isEmpty()) {
+                    loginPassword.setError("Password is required");
+                    return;
+                }
 
+                loginUser(email, password);
             }
         });
-        signUpBtn.setOnClickListener(new View.OnClickListener() {
+
+        signupRedirectText.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 startActivity(new Intent(Login.this, Register.class));
             }
         });
     }
 
-    public void signIn(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+    private void loginUser(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            if (user != null && user.isEmailVerified()) {
+                                String userName = user.getDisplayName();
+                                String userEmail = user.getEmail();
+                                saveUserData(userName, userEmail);
+
+                                Intent profileIntent = new Intent(Login.this, MainActivity.class);
+                                profileIntent.putExtra("userName", userName);
+                                profileIntent.putExtra("userEmail", userEmail);
+                                startActivity(profileIntent);
+                                finish();
+                            } else {
+                                Toast.makeText(Login.this, "Please verify your email!", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            Toast.makeText(Login.this, "Authentication failed: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void saveUserData(String userName, String userEmail) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("userName", userName);
+        editor.putString("userEmail", userEmail);
+
+        editor.putBoolean("isLoggedIn", true);
+        editor.apply();
+    }
+
+    private boolean isLoggedIn() {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        return preferences.getBoolean("isLoggedIn", false);
+    }
+
+    private void navigateToMainPage() {
+        startActivity(new Intent(Login.this, MainActivity.class));
+        finish();
     }
 }
--- app/src/main/java/com/example/galaxyofmemories/Register.java
+++ app/src/main/java/com/example/galaxyofmemories/Register.java
@@ -1,100 +1,120 @@
 package com.example.galaxyofmemories;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatButton;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class Register extends AppCompatActivity {
 
-    private boolean passwordShowing = false;
-    private boolean conPasswordShowing = false;
+    EditText username, registerEmail, registerPassword, registerConfirmPassword;
+    Button registerButton;
+    FirebaseAuth mAuth;
+
+    TextView loginText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        mAuth = FirebaseAuth.getInstance();
 
-        final EditText email = findViewById(R.id.emailET);
-        final EditText mobile = findViewById(R.id.mobileET);
+        username = findViewById(R.id.username);
+        registerEmail = findViewById(R.id.email);
+        registerPassword = findViewById(R.id.password);
+        registerConfirmPassword = findViewById(R.id.confirm_password);
+        registerButton = findViewById(R.id.signupBtn);
+        loginText = findViewById(R.id.signupText);
 
-        final EditText password = findViewById(R.id.passwordET);
-        final EditText conPassword = findViewById(R.id.conpasswordET);
-        final ImageView passwordIcon = findViewById(R.id.passwordIcon);
-        final ImageView conPasswordIcon = findViewById(R.id.conPasswordIcon);
-
-        final AppCompatButton signUpBtn = findViewById(R.id.signUpBtn);
-        final TextView signInBtn = findViewById(R.id.signIpBtn);
-
-        passwordIcon.setOnClickListener(new View.OnClickListener() {
+        registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
+                final String name = username.getText().toString().trim();
+                final String email = registerEmail.getText().toString().trim();
+                final String password = registerPassword.getText().toString().trim();
+                String confirmPassword = registerConfirmPassword.getText().toString().trim();
 
-                if (passwordShowing) {
-                    passwordShowing = false;
-
-                    password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
-                    passwordIcon.setImageResource(R.drawable.password_show);
-                } else {
-                    passwordShowing = true;
-
-                    password.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
-                    passwordIcon.setImageResource(R.drawable.password_hide);
+                if (name.isEmpty() || email.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
+                    Toast.makeText(Register.this, "All fields are required", Toast.LENGTH_SHORT).show();
+                    return;
                 }
 
-                password.setSelection(password.length());
-
-            }
-        });
-
-        conPasswordIcon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (conPasswordShowing) {
-                    conPasswordShowing = false;
-
-                    conPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
-                    conPasswordIcon.setImageResource(R.drawable.password_show);
-                } else {
-                    conPasswordShowing = true;
-
-                    conPassword.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
-                    passwordIcon.setImageResource(R.drawable.password_hide);
+                if (!password.equals(confirmPassword)) {
+                    Toast.makeText(Register.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+                    return;
                 }
 
-                conPassword.setSelection(conPassword.length());
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(Register.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    sendEmailVerification(name, email);
+                                } else {
+                                    try {
+                                        throw task.getException();
+                                    } catch (FirebaseAuthWeakPasswordException e) {
+                                        Toast.makeText(Register.this, "Weak Password", Toast.LENGTH_SHORT).show();
+                                    } catch (FirebaseAuthInvalidCredentialsException e) {
+                                        Toast.makeText(Register.this, "Invalid Email", Toast.LENGTH_SHORT).show();
+                                    } catch (FirebaseAuthUserCollisionException e) {
+                                        Toast.makeText(Register.this, "User with this email already exists", Toast.LENGTH_SHORT).show();
+                                    } catch (Exception e) {
+                                        Log.e("FirebaseError", "Error: " + e.getMessage());
+                                        Toast.makeText(Register.this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            }
+                        });
             }
         });
-        signUpBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                final String getMobileTxt = mobile.getText().toString();
-                final String getEmailTxt = email.getText().toString();
-
-                Intent intent = new Intent(Register.this, OTPVerification.class);
-
-                intent.putExtra("mobile", getMobileTxt);
-                intent.putExtra("email", getEmailTxt);
-
-                startActivity(intent);
-
-            }
-        });
-
-        signInBtn.setOnClickListener(new View.OnClickListener() {
+        loginText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(Register.this, Login.class));
             }
         });
+    }
 
+    private void sendEmailVerification(final String name, final String email) {
+        mAuth.getCurrentUser().sendEmailVerification()
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(Register.this, "Verification email sent to " + mAuth.getCurrentUser().getEmail(), Toast.LENGTH_SHORT).show();
+
+                            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                    .setDisplayName(name)
+                                    .build();
+                            mAuth.getCurrentUser().updateProfile(profileUpdates);
+                            Intent intent = new Intent(Register.this, Login.class);
+                            startActivity(intent);
+                        } else {
+                            Log.e("EmailVerification", "sendEmailVerification", task.getException());
+                            Toast.makeText(Register.this, "Failed to send verification email", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
