b4689699a615fd1629c53bdd45ff8a9be545807d
tat3ka07 <tatamiraghyan.7@gmail.com>
2024-05-31 23:49:58 +0400

    ++

--- app/src/main/java/com/example/galaxyofmemories/Birthday.java
+++ app/src/main/java/com/example/galaxyofmemories/Birthday.java
@@ -1,14 +1,90 @@
 package com.example.galaxyofmemories;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
-public class Birthday extends AppCompatActivity {
+import com.example.galaxyofmemories.activities.CreateNoteActivity;
+import com.example.galaxyofmemories.adapters.NotesAdapter;
+import com.example.galaxyofmemories.database.NotesDatabase;
+import com.example.galaxyofmemories.entities.Note;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Birthday extends AppCompatActivity implements NotesListener {
+    public static final int REQUEST_CODE_ADD_NOTE = 1;
+    public static final int REQUEST_CODE_UPDATE_NOTE = 2;
+    public static final int REQUEST_CODE_SHOW_NOTES = 3;
+    public static final int REQUEST_CODE_SELECT_IMAGE = 4;
+    public static final int REQUEST_CODE_STORAGE_PERMISSION = 4;
+    private int noteClickedPosition = -1;
+
+
+    private RecyclerView notesRecyclerView;
+    private List<Note> noteList;
+    private NotesAdapter notesAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_birthday);
+        notesRecyclerView = findViewById(R.id.notesRecyclerView);
+        notesRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
+
+        noteList = new ArrayList<>();
+        notesAdapter = new NotesAdapter(noteList, this);
+        notesRecyclerView.setAdapter(notesAdapter);
+
+        getNotes(REQUEST_CODE_SHOW_NOTES, false);
+    }
+    private void getNotes(final int requestCode, final boolean isNoteDeleted) {
+
+        @SuppressLint("StaticFieldLeak")
+        class GetNotesTask extends AsyncTask<Void, Void, List<Note>> {
+
+            @Override
+            protected List<Note> doInBackground(Void... voids) {
+                return NotesDatabase.getDatabase(getApplicationContext()).noteDao().getAllNotes();
+            }
+
+            @SuppressLint("NotifyDataSetChanged")
+            @Override
+            protected void onPostExecute(List<Note> notes) {
+                super.onPostExecute(notes);
+                if (requestCode == REQUEST_CODE_SHOW_NOTES) {
+                    noteList.addAll(notes.stream().filter(el -> "#F43636".equalsIgnoreCase(el.getColor())).collect(Collectors.toList()));
+                    notesAdapter.notifyDataSetChanged();
+                } else if (requestCode == REQUEST_CODE_ADD_NOTE) {
+                    noteList.add(0, notes.get(0));
+                    notesAdapter.notifyItemInserted(0);
+                    notesRecyclerView.smoothScrollToPosition(0);
+                } else if (requestCode == REQUEST_CODE_UPDATE_NOTE) {
+                    noteList.remove(noteClickedPosition);
+                    if (isNoteDeleted) {
+                        notesAdapter.notifyItemRemoved(noteClickedPosition);
+                    } else {
+                        noteList.add(noteClickedPosition, notes.get(noteClickedPosition));
+                        notesAdapter.notifyItemChanged(noteClickedPosition);
+                    }
+                }
+            }
+        }
+
+        new GetNotesTask().execute();
+    }
+
+    @Override
+    public void onNoteClicked(Note note, int position) {
+        noteClickedPosition = position;
+        Intent intent = new Intent(getApplicationContext(), CreateNoteActivity.class);
+        intent.putExtra("isViewOrUpdate", true);
+        intent.putExtra("note", note);
+        startActivityForResult(intent, REQUEST_CODE_UPDATE_NOTE);
     }
 }
